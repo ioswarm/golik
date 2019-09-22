d48a40701bbdeea800292c847b2ed5dc37e34a60
Andreas Pape <andreas@ioswarm.de>
2019-09-22 21:20:42 +0200

    simple pub-sub example

---
+++ example/pubsub/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"time"
+	"github.com/ioswarm/golik"
+)
+
+type Ticker struct {
+	ticker *time.Ticker
+}
+
+func (t* Ticker)PostStart(c golik.CloveRef) {
+	t.ticker = c.Ticker(2*time.Second, "Ping")
+}
+
+func (t* Ticker)PreStop() {
+	t.ticker.Stop()
+}
+
+func (t* Ticker)PingReceive(s string, c golik.CloveRef) {
+	c.Info("Publish %v", s)
+	c.Publish(s)
+}
+
+type Receiver struct {}
+
+func (r *Receiver)PostStart(c golik.CloveRef) {
+	c.Subscribe(c, func(val interface{}) bool {
+		switch val.(type) {
+		case string: return true
+		default: return false
+		}
+	})
+}
+
+func (r* Receiver)Receive(s string, c golik.CloveRef) {
+	c.Info("Receive %v", s)
+}
+
+// TODO explain in more detail
+func main() {
+	system := golik.GolikSystem()
+
+	system.Of(golik.Minion(func() interface{} { return &Ticker{} } ), "ticker")
+	system.Of(golik.Minion(func() interface{} { return &Receiver{} }), "receiver")
+
+	<- system.Terminated()
+}
