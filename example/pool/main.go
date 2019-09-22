61c3bfd7c33485535b497d56ddf088b82c1829a7
Andreas Pape <andreas@ioswarm.de>
2019-09-22 21:20:15 +0200

    simple demonstration of a worker-pool

---
+++ example/pool/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/ioswarm/golik"
+)
+
+type Worker struct{
+
+}
+
+func (w *Worker) HandleString(s string, c golik.CloveRef) {
+	time.Sleep(1 * time.Second)
+	c.Info("Receive: %v", s)
+}
+
+func NewWorker() interface{} {
+	return &Worker{}
+}
+
+// TODO explain in more detail
+func main() {
+	system := golik.GolikSystem()
+
+	ref := system.Of(golik.WorkerPool(10, golik.Minion(NewWorker)), "pool")
+
+	for i := 0; i < 100; i++ {
+		//time.Sleep(250 * time.Millisecond)
+		ref.Tell(fmt.Sprintf("STRING-VALUE -> %v", i))
+	}
+
+	<-system.Terminated()
+}
