710f88fd9505b813a6aaabc4dc45826eda84675c
ioswarm <andreas@ioswarm.de>
2020-10-13 12:07:20 +0200

    use pointer

--- persistance/handler.go
+++ persistance/handler.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Handler interface {
-	Filter(golik.CloveContext, filter.Filter) (filter.Result, error)
+	Filter(golik.CloveContext, *filter.Filter) (*filter.Result, error)
 
-	Create(golik.CloveContext, Create) error
-	Read(golik.CloveContext, Get) (interface{}, error)
-	Update(golik.CloveContext, Update) error
+	Create(golik.CloveContext, *Create) error
+	Read(golik.CloveContext, *Get) (interface{}, error)
+	Update(golik.CloveContext, *Update) error
 	// TODO Patch
-	Delete(golik.CloveContext, Delete) (interface{}, error)
+	Delete(golik.CloveContext, *Delete) (interface{}, error)
 
 	OrElse(golik.CloveContext, msg golik.Message)
 }
