4c8017eb8d9ce8b56f8a5cc51b34d85c7730e23d
ioswarm <andreas@ioswarm.de>
2020-10-13 14:46:49 +0200

    bug fix

--- persistance/pool.go
+++ persistance/pool.go
@@ -2,6 +2,7 @@ package persistance
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/ioswarm/golik"
 )
@@ -14,13 +15,13 @@ type ConnectionPoolSettings interface {
 	Connect(golik.CloveContext) error
 	Close(golik.CloveContext) error
 
-	CreateHandler(golik.CloveContext) (Handler, error) 
+	CreateHandler(golik.CloveContext) (Handler, error)
 }
 
-func NewConnectionPool(settings ConnectionPoolSettings) *Clove {
+func NewConnectionPool(settings ConnectionPoolSettings) *golik.Clove {
 	return &golik.Clove{
-		Name: settings.Name(),
-		Sync: true,
+		Name:     settings.Name(),
+		Sync:     true,
 		PreStart: settings.Connect,
 		PostStop: settings.Close,
 		Behavior: func(ctx golik.CloveContext, msg golik.Message) {
@@ -41,12 +42,13 @@ func NewConnectionPool(settings ConnectionPoolSettings) *Clove {
 					return err
 				}
 				if _, err := ctx.Execute(&golik.Clove{
-					Name: fmt.Sprintf("%v-%v", settings.Name(), i),
+					Name:     fmt.Sprintf("%v-%v", settings.Name(), i),
 					Behavior: handler,
 				}); err != nil {
 					return err
 				}
 			}
+			return nil
 		},
 	}
 }
