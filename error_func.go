9feed27a0611e12b5fcaa7b15e85ce562ec2499c
ioswarm <andreas@ioswarm.de>
2020-10-16 10:56:16 +0200

    extend Error

--- error_func.go
+++ error_func.go
@@ -1,14 +1,60 @@
 package golik
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
 
 func (e *Error) Error() string {
 	return fmt.Sprintln(e.Code, e.Message)
 }
 
+func (e *Error) WithCode(code string) *Error {
+	e.Code = code
+	return e
+}
+
+func (e *Error) WithDescription(description string) *Error {
+	e.Description = description
+	return e
+}
+
+func (e *Error) WithMeta(key string, value string) *Error {
+	if e.Meta == nil {
+		e.Meta = make(map[string]string)
+	}
+	e.Meta[key] = value
+	return e
+}
+
+func (e *Error) MetaValue(key string) (string, bool) {
+	value, ok := e.Meta[key]
+	return value, ok
+}
+
+func (e *Error) WithHttpStatus(status int) *Error {
+	return e.WithMeta("http.status", strconv.Itoa(status))
+}
+
+func (e *Error) HttpStatus() int {
+	if value, ok := e.MetaValue("http.status"); ok {
+		if status, err := strconv.Atoi(value); err == nil {
+			return status
+		}
+		return 500
+	}
+	return 200
+}
+
+
+
 func NewError(message string) *Error {
 	return &Error{
 		Message: message,
+		Timestamp: timestamppb.Now(),
+		Meta: make(map[string]string),
 	}
 }
 
