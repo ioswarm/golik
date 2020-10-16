e688d67fac24ab653c624b5c4776497f64203bba
ioswarm <andreas@ioswarm.de>
2020-10-16 17:22:16 +0200

    if Filter is zero-value than alwas check with true

--- filter_commands.go
+++ filter_commands.go
@@ -1,21 +1,22 @@
 package golik
 
 type Filter struct {
-	Filter string `json:"filter,omitempty"`
-	From int `json:"from,omitempty"`
-	Size int `json:"size,omitempty"`
-	Meta map[string]string `json:"meta,omitempty"`
+	Filter string            `json:"filter,omitempty"`
+	From   int               `json:"from,omitempty"`
+	Size   int               `json:"size,omitempty"`
+	Meta   map[string]string `json:"meta,omitempty"`
 }
 
 func (f *Filter) Condition() (Condition, error) {
+	if f.Filter == "" {
+		return EmptyCondition(), nil
+	}
 	return Parse(f.Filter)
 }
 
-
 type Result struct {
-	From int `json:"from"`
-	Size int `json:"size,omitempty"`
-	Count int `json:"count,omitempty"`
+	From   int           `json:"from"`
+	Size   int           `json:"size,omitempty"`
+	Count  int           `json:"count,omitempty"`
 	Result []interface{} `json:"result"`
 }
-
